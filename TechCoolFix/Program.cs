bc33727c752000d3d623529fe02f5331a26ccc28
August2211 <53059031+August2211@users.noreply.github.com>
2023-04-14 09:37:02 +0200

    Added files to run techcool

---
+++ TechCoolFix/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+using TECHCOOL.UI;
+
+namespace TechCoolFix
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            UITest uITest = new UITest();
+            Screen.Display(uITest);
+        }
+    }
+}
---
+++ TechCoolFix/UITest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TECHCOOL.UI;
+
+namespace TechCoolFix
+{
+    public class UITest : Screen
+    {
+        public override string Title { get; set; } = "Products";
+
+        protected override void Draw()
+        {
+            var cursorpos = Console.GetCursorPosition();
+            Clear(this);
+            Console.SetCursorPosition(0, 0);
+            int consoleLength = Console.WindowHeight;
+            ListPage<UITest> list = new ListPage<UITest>();
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add(new UITest() { Title = $"Product {i}" });
+            }
+
+            list.AddColumn("Title", "Title");
+            list.Select();
+
+
+            Console.SetCursorPosition(0, Console.CursorTop); // set cursor position to the end of the last line
+
+        }
+    }
+}
